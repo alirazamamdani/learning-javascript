c3db12152d09744b0810c9fcb10c39b772e193c2
alirazamamdani <ali.mamdani@Hotmail.com>
2021-04-09 19:01:29 +0500

    learning Javascript

---
+++ app.js
@@ -0,0 +1,104 @@
+// alert("Welcome to JS Land....");
+
+// alert("Error! Please enter a valid password.");
+
+// alert(`Welcome to JS Land...\nHappy Coding`);
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding");
+
+
+// alert("Hello... I can run JS through my web browser's console")
+
+
+// var username;
+// var myName = "ALi Raza Mamdani";
+// var message = "Hello World";
+// alert(message)
+
+// var studentName = "Jhone Doe";
+// var studentAge = 15;
+// var studentCourse = "Certified Mobile Application Development"
+
+// alert(studentName)
+// alert(studentAge + " years old")
+// alert(studentCourse)
+
+// var eatFood = "PIZZA\nPIZZ\nPIZ\n\PI\nP"
+// alert(eatFood)
+
+// var email = "Alimamdani@Hotmail.com";
+// alert("My email addres is: " + email)
+
+// var book = "A smarter way to learn Javascript";
+// alert("I am trying to learn from the "+ book)
+
+// var note = "Yah! I can write HTML content through Javascript";
+// document.write(note)
+
+// var something = "----------$#&#$----------";
+// alert(something)
+
+
+// var age = 24;
+// alert("I am "+ age + " years Old")
+
+// var visitor = 10;
+// alert("You have visited this " + visitor + " times")
+
+// var companyName = "<b>www.xyzClothing.com</b>"
+// var visitorName = "<b>Ali Raza Mamdani</b>";
+// var productTitle = "<b>Jean's Pant</b>";
+// var quantity = 7;
+// document.write( visitorName + " ordered " +  quantity + " " + productTitle + " on " + companyName)
+
+
+// var one , two , three;
+
+    // var a,a_,_a,cx,s2;
+    // var 1a,#a,$ac,.x,;
+
+// document.write("<h1>Rules for naming JS Variables</h1>")
+// document.write("Variable names can only contain Letters, Numbers , Underscore , dollar sign <br>")
+// document.write("Variable must begin with a Letter , underscore or dollars signs <br>");
+// document.write("Variables names are case sensitive <br>");
+// document.write("Variable names should not be JS keywords")
+
+
+// var one = +prompt("Enter first number")
+// var two = +prompt("Emter Second numbers");
+// var finalAnswer = one + two;
+
+// document.write("Sum of "+ one + " and " + two + " is "+ finalAnswer)
+
+// var one = +prompt("Enter first number")
+// var two = +prompt("Emter Second numbers");
+// var finalAnswer = one - two;
+
+// document.write("Sum of "+ one + " and " + two + " is "+ finalAnswer)
+
+// var one = +prompt("Enter first number")
+// var two = +prompt("Emter Second numbers");
+// var finalAnswer = one * two;
+
+// document.write("Sum of "+ one + " and " + two + " is "+ finalAnswer)
+
+// var one = +prompt("Enter first number")
+// var two = +prompt("Emter Second numbers");
+// var finalAnswer = one / two;
+
+// document.write("Sum of "+ one + " and " + two + " is "+ finalAnswer)
+
+// var one = +prompt("Enter first number")
+// var two = +prompt("Emter Second numbers");
+// var finalAnswer = one % two;
+
+// document.write("Sum of "+ one + " and " + two + " is "+ finalAnswer)
+
+
+var random;
+document.write("Value after variable declaration is "+ random + "<br>")
+random = 5;
+document.write("Initial Value: "+ random+ " <br>")
+var randomadd = ++random;
+document.write("Value after increment :"+ randomadd);
